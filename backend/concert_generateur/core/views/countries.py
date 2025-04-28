e48f5466b4cb03c0e2fd62b920cb501893b38fb5
Aurélien <aurelienpenotperbet@gmail.com>
2025-04-28 18:44:58 +0200

    feat: add countries route && erase pagination

--- backend/concert_generateur/core/views/countries.py
+++ backend/concert_generateur/core/views/countries.py
@@ -5,15 +5,21 @@ from rest_framework.permissions import IsAdminUser
 from core.models.countries import Country
 from core.serializers.countries import CountrySerializer
 from core.permission import HasPermissions
-from core.views.base import BaseSafeModelViewSet, BaseSafeRetrieveAPIView
+from core.views.base import BaseSafeModelViewSet, BaseSafeRetrieveAPIView, BaseSafeListAPIView
 
 class CountryAdminViewSet(BaseSafeModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     permission_classes = [IsAdminUser]
 
-class CountryReadOnlyView(BaseSafeRetrieveAPIView):
+class CountryListView(BaseSafeListAPIView):
     queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = [HasPermissions]
+    required_permissions = ['core.view_country']
+    permission_logic = 'all'
+
+class CountryReadOnlyView(BaseSafeRetrieveAPIView):
     serializer_class = CountrySerializer
     permission_classes = [HasPermissions]
     required_permissions = ['core.view_country']
